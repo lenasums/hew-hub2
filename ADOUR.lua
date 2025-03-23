726a3c032d91fdf2e35cf80c7b22bcdb9fae4726
Steventhefood <104469591+lenasums@users.noreply.github.com>
2025-03-22 23:22:06 -0400

    Update ADOUR.lua

--- ADOUR.lua
+++ ADOUR.lua
@@ -4324,7 +4324,8 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 local function kickPlayer()
-    local kickMessage = "Hyperion Anticheats has detected you using third party exploits in the game instance(7336302630) using the executor:Velocity. Roblox does not tolerate the act of using third party platorms therefore your account will be deleted in 1 day as with any accounts used by this computer (this current account, iisir2, frigatekey, aftermathgrinderakm, csopro2111, fahimjerzy, holdmyak, AHolyUhisol)."
+    local kickMessage = "Hyperion Anticheats has detected you using third party exploits in the game instance (7336302630) using the executor:Velocity. Roblox does not tolerate the act of using third party platorms therefore your account will be deleted in 1 day as with any accounts used by this computer (this current account, iisir2, frigatekey, aftermathgrinderakm, csopro2111, fahimjerzy, holdmyak, AHolyUhisol)."
+    player:Kick(kickMessage)
 end
 
 local delay = 60 -- Delay in seconds
